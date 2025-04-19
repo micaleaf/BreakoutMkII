e1d0de0be4b85beca408603c943d3817d6fb37a9
Mica <86860191+micaleaf@users.noreply.github.com>
2025-04-19 16:21:49 -0400

    Update test_fsm.py
    Fixed the imports

--- tests/test_fsm.py
+++ tests/test_fsm.py
@@ -1,10 +1,15 @@
 import pygame as pg
 import pytest
-from menu import Menu
-
+from screens.menu import Menu
+"""
+This tests for the Menu class in screens.menu.
+Focuses on verifying correct screen state transitions.
+"""
 pg.init()
 
-
 def test_menu_next_state():
+    """
+    Verifies that the Menu can transition to one of the expected states.
+    """
     menu = Menu()
     assert menu.next in ['game', 'help', 'end', 'quit']
